bd4334873e8bb7146c4c7ab1aa37625cc857de22
Sergey Yevtushok <serhiiyevtushok@Serhiis-MacBook-Pro.local>
2022-04-29 07:49:09 +0300

    binary implementation

--- src/main/java/examples/data/structure/Node.java
+++ src/main/java/examples/data/structure/Node.java
@@ -1,2 +1,18 @@
-package examples.data.structure;public class Node {
+package examples.data.structure;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Node {
+    private int value;
+    private Node left;
+    private Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
 }
