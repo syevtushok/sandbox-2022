88739157d194be8a114538563622494b9f3405dc
serhiiyevtushok <ggazila@gmail.com>
2022-07-10 14:05:46 +0300

    custom json parser

--- src/main/java/examples/json/jackson/JacksonExample.java
+++ src/main/java/examples/json/jackson/JacksonExample.java
@@ -1,2 +1,44 @@
-package examples.json.jackson;public class JacksonExample {
+package examples.json.jackson;
+
+import java.io.File;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class JacksonExample {
+    public static void main(String[] args) {
+        objectToJson();
+        jsonToObject(Car.class);
+    }
+
+    @SneakyThrows
+    private static <T> void jsonToObject(Class<T> carClass) {
+        String json = "{ \"color\" : \"Black\", \"type\" : \"BMW\", \"year\" : \"2015\" }";
+        T t = new ObjectMapper().readValue(json, carClass);
+        log.info("car - {}", t);
+    }
+
+    @SneakyThrows
+    private static void objectToJson() {
+        Car car = new Car("red", "crossover", 2015);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(new File("target/car.json"), car);
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Car {
+
+    public String color;
+    public String type;
+    public Integer year;
 }
--- src/main/java/examples/project/json_parser/JsonParser.java
+++ src/main/java/examples/project/json_parser/JsonParser.java
@@ -1,2 +1,62 @@
-package examples.project.json_parser;public class JsonParser {
+package examples.project.json_parser;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import lombok.SneakyThrows;
+import lombok.ToString;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@ToString
+public class JsonParser {
+    public static void main(String[] args) {
+        String json = "{\"color\":\"red\",\"type\":\"crossover\",\"year\":2015}";
+        Car car = JsonInternalParser.parseToObject(json, Car.class);
+        JsonParser jsonParser = JsonInternalParser.parseToObject(json, JsonParser.class);
+        log.info("car - {}", car);
+        log.info("jsonParser - {}", jsonParser);
+    }
+
+    static class JsonInternalParser {
+
+        @SneakyThrows
+        static <T> T parseToObject(String json, Class<T> object) {
+            T genericObject = object.getDeclaredConstructor().newInstance();
+            Field[] fields = genericObject.getClass().getFields();
+
+            Map<String, String> mapFieldValue = mapFieldValue(json);
+
+            for (Field field : fields) {
+                if (mapFieldValue.containsKey(field.getName())) {
+                    String type = field.getType().getName();
+                    String value = mapFieldValue.get(field.getName());
+                    if (type.equals("java.lang.String")) {
+                        field.set(genericObject, value);
+                    } else if (type.equals("java.lang.Integer")) {
+                        field.set(genericObject, Integer.valueOf(value));
+                    }
+                }
+            }
+
+            return genericObject;
+        }
+
+        private static Map<String, String> mapFieldValue(String json) {
+            return Arrays.stream(json.split(","))
+                    .map(s -> s.replaceAll("[{}\"]", ""))
+                    .collect(Collectors.toMap(
+                            s -> s.substring(0, s.indexOf(":")).trim(),
+                            o -> o.substring(o.indexOf(":") + 1).trim()));
+        }
+    }
+}
+
+@ToString
+class Car {
+    public String color;
+    public String type;
+    public Integer year;
 }
