70352c32469885316ceca75576a44d8287eb682a
serhiiyevtushok <ggazila@gmail.com>
2022-07-20 20:40:04 +0300

    loader

--- src/main/java/examples/loaders/ClassLoaderExample.java
+++ src/main/java/examples/loaders/ClassLoaderExample.java
@@ -1,2 +1,11 @@
-package examples.loaders;public class ClassLoaderExample {
+package examples.loaders;
+
+import lombok.SneakyThrows;
+
+public class ClassLoaderExample {
+    @SneakyThrows
+    public static void main(String[] args) {
+        Class<?> aClass = ClassLoader.getSystemClassLoader().loadClass("examples.loaders.ClassLoaderExample");
+        System.out.println(aClass);
+    }
 }
